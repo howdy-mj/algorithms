4e182c0b6cdbc0a05e2d52bcb8b668cc30b99c06
howdy-mj <hi.minjungkim@gmail.com>
2022-11-27 20:01:41 +0900

    prettier 포맷팅

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,30 +1,28 @@
 import React from 'react';
 import styles from './styles.module.css';
-import Link from "@docusaurus/Link";
+import Link from '@docusaurus/Link';
 
 export default function HomepageFeatures(): JSX.Element {
   return (
     <section className={styles.features}>
-        <div className="container">
-            <div >
-                <h1>Theory</h1>
-                <Link
-                    className="button button--secondary button--lg"
-                    to="/docs/intro"
-                >
-                    Theory
-                </Link>
-            </div>
-
-            <div className={styles.content}>
-                <h1>Blog</h1>
-                <Link
-                    className="button button--secondary button--lg"
-                    to="/blog">
-                    Blog
-                </Link>
-            </div>
+      <div className="container">
+        <div>
+          <h1>Theory</h1>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro"
+          >
+            Theory
+          </Link>
         </div>
+
+        <div className={styles.content}>
+          <h1>Blog</h1>
+          <Link className="button button--secondary button--lg" to="/blog">
+            Blog
+          </Link>
+        </div>
+      </div>
     </section>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 export default function Home(): JSX.Element {
   return (
-    <Layout
-      title="Home"
-      >
+    <Layout title="Home">
       <main>
-          <HomepageFeatures />
+        <HomepageFeatures />
       </main>
     </Layout>
   );
